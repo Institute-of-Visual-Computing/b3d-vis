5d9d3029d5cda6ab3e4398c8cb94858f6fe242a0
David Eibich <d3ibich@gmail.com>
2024-01-16 10:04:24 +0100

    SimpleTriangles Cleanup and preparation for two cameras

--- renderer/simpleTriangles/SimpleTrianglesRenderer.h
+++ renderer/simpleTriangles/SimpleTrianglesRenderer.h
@@ -10,6 +10,7 @@ struct NativeCube
 	owl::vec3f scale;
 	owl::Quaternion3f rotation;
 };
+
 class SimpleTrianglesRenderer final : public RendererBase
 {
 public:
@@ -19,12 +20,14 @@ protected:
 	auto onRender(const View& view) -> void override;
 	auto onInitialize() -> void override;
 
-	owl::AffineSpace3f trs;
-
+	owl::AffineSpace3f trs_;
 	bool sbtDirty = true;
-	OWLRayGen rayGen{ 0 };
-	OWLContext context{ 0 };
+	owl2i fbSize_ { 0, 0 };
+
+	OWLRayGen rayGen_{ nullptr };
+	OWLMissProg missProg_{ nullptr };
+	OWLContext context_{ nullptr };
 	OWLBuffer surfaceBuffer_{ nullptr };
-	OWLParams launchParameters_{ nullptr };
+	OWLParams launchParameters_{ };
 	OWLGroup world_{ nullptr };
 };
--- renderer/simpleTriangles/simpleTrianglesHostCode.cpp
+++ renderer/simpleTriangles/simpleTrianglesHostCode.cpp
@@ -23,34 +23,70 @@ namespace
 	{
 		return max(max(computeStableEpsilon(v.x), computeStableEpsilon(v.y)), computeStableEpsilon(v.z));
 	}
+
+	struct GuiData
+	{
+		struct BackgroundColorPalette
+		{
+			std::array<float, 4> color1{ 0.572f, 0.100f, 0.750f, 1.0f };
+			std::array<float, 4> color2{ 0.0f, 0.3f, 0.3f, 1.0f };
+		};
+		BackgroundColorPalette rtBackgroundColorPalette;
+
+		struct CubeVolumeTransform
+		{
+			std::array<float, 3> position{ 0, 0, 0 };
+			std::array<float, 3> scale{ 1, 1, 1 };
+			std::array<float, 3> rotation{ 0, 0, 0 };
+		};
+		CubeVolumeTransform rtCubeVolumeTransform{};
+	};
+
+	RayCameraData createRayCameraData(const Camera& camera, const Extent& textureExtent)
+	{
+		const auto origin = vec3f{ camera.origin.x, camera.origin.y, camera.origin.z };
+		auto camera_d00 = camera.at - origin;
+		const auto wlen = length(camera_d00);
+
+		const auto aspect = textureExtent.width / static_cast<float>(textureExtent.height);
+		const auto vlen = wlen * std::cosf(0.5f * camera.FoV);
+		const auto camera_ddu = vlen * aspect * normalize(cross(camera_d00, camera.up));
+		const auto camera_ddv = vlen * normalize(cross(camera_ddu, camera_d00));
+
+		const auto vz = -normalize(camera.at - origin);
+		const auto vx = normalize(cross(camera.up, vz));
+		const auto vy = normalize(cross(vz, vx));
+		const auto focalDistance = length(camera.at - origin);
+		const float minFocalDistance = max(computeStableEpsilon(origin), computeStableEpsilon(vx));
+
+		float screen_height = 2.f * tanf(camera.FoV / 2.f) * max(minFocalDistance, focalDistance);
+		const auto vertical = screen_height * vy;
+		const auto horizontal = screen_height * aspect * vx;
+		const auto lower_left = -max(minFocalDistance, focalDistance) * vz - 0.5f * vertical - 0.5f * horizontal;
+
+		return { origin, lower_left, horizontal, vertical };
+	}
+
+	GuiData guiData{};
+	
+	const int NUM_VERTICES = 8;
+	vec3f vertices[NUM_VERTICES] = { { -0.5f, -0.5f, -0.5f }, { +0.5f, -0.5f, -0.5f }, { -0.5f, +0.5f, -0.5f },
+									 { +0.5f, +0.5f, -0.5f }, { -0.5f, -0.5f, +0.5f }, { +0.5f, -0.5f, +0.5f },
+									 { -0.5f, +0.5f, +0.5f }, { +0.5f, +0.5f, +0.5f } };
+
+	const int NUM_INDICES = 12;
+	vec3i indices[NUM_INDICES] = { { 0, 1, 3 }, { 2, 3, 0 }, { 5, 7, 6 }, { 5, 6, 4 }, { 0, 4, 5 }, { 0, 5, 1 },
+								   { 2, 3, 7 }, { 2, 7, 6 }, { 1, 5, 7 }, { 1, 7, 3 }, { 4, 0, 2 }, { 4, 2, 6 } };
+
 }
-
-const int NUM_VERTICES = 8;
-vec3f vertices[NUM_VERTICES] = {
-	{ -0.5f, -0.5f, -0.5f }, { +0.5f, -0.5f, -0.5f }, { -0.5f, +0.5f, -0.5f }, { +0.5f, +0.5f, -0.5f },
-	{ -0.5f, -0.5f, +0.5f }, { +0.5f, -0.5f, +0.5f }, { -0.5f, +0.5f, +0.5f }, { +0.5f, +0.5f, +0.5f }
-};
-
-const int NUM_INDICES = 12;
-vec3i indices[NUM_INDICES] = { { 0, 1, 3 }, { 2, 3, 0 }, { 5, 7, 6 }, { 5, 6, 4 }, { 0, 4, 5 }, { 0, 5, 1 },
-							   { 2, 3, 7 }, { 2, 7, 6 }, { 1, 5, 7 }, { 1, 7, 3 }, { 4, 0, 2 }, { 4, 2, 6 } };
-
-
-// const vec2i fbSize(800,600);
-const vec3f init_lookFrom(-4.f, +3.f, -2.f);
-const vec3f init_lookAt(0.f, 0.f, 0.f);
-const vec3f init_lookUp(0.f, 1.f, 0.f);
-const float init_cosFovy = 0.66f;
-
 
 auto SimpleTrianglesRenderer::setCubeVolumeTransform(NativeCube* nativeCube) -> void
 {
 	auto translate = affine3f::translate(nativeCube->position);
 	auto scale = affine3f::scale(nativeCube->scale);
-
 	AffineSpace3f rotate{ nativeCube->rotation };
-	// trs = rotate * translate * scale;
-	trs = translate * rotate * scale;
+
+	trs_ = translate * rotate * scale;
 }
 
 auto SimpleTrianglesRenderer::onRender(const View& view) -> void
@@ -60,9 +96,7 @@ auto SimpleTrianglesRenderer::onRender(const View& view) -> void
 	waitParams.params.fence.value = view.fenceValue;
 	cudaWaitExternalSemaphoresAsync(&initializationInfo_.signalSemaphore, &waitParams, 1);
 
-
 	// map/create/set surface
-	// TODO: class members
 	std::array<cudaArray_t, 2> cudaArrays{};
 	std::array<cudaSurfaceObject_t, 2> cudaSurfaceObjects{};
 
@@ -81,49 +115,38 @@ auto SimpleTrianglesRenderer::onRender(const View& view) -> void
 		}
 	}
 	
-	// const auto nativeDeviceBufferPtr = owlBufferGetPointer(surfaceBuffer_, 0);
-	owl2i fbSize = {
-		static_cast<int32_t>(view.colorRt.extent.width),
-		static_cast<int32_t>(view.colorRt.extent.height)
-	};
-	owlRayGenSet2i(rayGen, "fbSize", fbSize);
-	
-	// ----------- compute variable values  ------------------
-	const auto& camera = view.cameras.front();
-	const auto origin = vec3f{ camera.origin.x, camera.origin.y, camera.origin.z };
-	auto camera_d00 = camera.at - origin;
-	const auto wlen = length(camera_d00);
-	
-	const auto aspect = view.colorRt.extent.width / static_cast<float>(view.colorRt.extent.height);
-	const auto vlen = wlen * std::cosf(0.5f * camera.FoV);
-	const auto camera_ddu = vlen * aspect * normalize(cross(camera_d00, camera.up));
-	const auto camera_ddv = vlen * normalize(cross(camera_ddu, camera_d00));
+	if (fbSize_.x != view.colorRt.extent.width || fbSize_.y != view.colorRt.extent.height)
+	{
+		fbSize_ = { static_cast<int32_t>(view.colorRt.extent.width), static_cast<int32_t>(view.colorRt.extent.height) };
+		owlRayGenSet2i(rayGen_, "fbSize", fbSize_);
+		sbtDirty = true;
+	}
 
-	const auto vz = -normalize(camera.at - origin);
-	const auto vx = normalize(cross(camera.up, vz));
-	const auto vy = normalize(cross(vz, vx));
-	const auto focalDistance = length(camera.at - origin);
-	const float minFocalDistance = max(computeStableEpsilon(origin), computeStableEpsilon(vx));
+	if (sbtDirty)
+	{
+		owlBuildSBT(context_);
+		sbtDirty = false;
+	}
 
-	float screen_height = 2.f * tanf(camera.FoV / 2.f) * max(minFocalDistance, focalDistance);
-	const auto vertical = screen_height * vy;
-	const auto horizontal = screen_height * aspect * vx;
-	const auto lower_left = -max(minFocalDistance, focalDistance) * vz - 0.5f * vertical - 0.5f * horizontal;
-
-	const RayCameraData rcd = { origin, lower_left, horizontal, vertical };
-
-	owlParamsSetRaw(launchParameters_, "cameraData", &rcd);
-	owlParamsSetRaw(launchParameters_, "surfacePointer", &cudaSurfaceObjects[0]);
-
-	owlInstanceGroupSetTransform(world_, 0, (const float*)&trs);
+	owlInstanceGroupSetTransform(world_, 0, (const float*)&trs_);
 	owlGroupRefitAccel(world_);
-	owlBuildSBT(context);
-	
-	sbtDirty = true;
 
-	owlAsyncLaunch2D(rayGen, view.colorRt.extent.width, view.colorRt.extent.height, launchParameters_);
+	{
+		const auto rcd = createRayCameraData(view.cameras[0], view.colorRt.extent);
+		owlParamsSetRaw(launchParameters_, "cameraData", &rcd);
+		owlParamsSetRaw(launchParameters_, "surfacePointer", &cudaSurfaceObjects[0]);
+		owlAsyncLaunch2D(rayGen_, view.colorRt.extent.width, view.colorRt.extent.height, launchParameters_);
+	}
+
+	if(view.mode == RenderMode::stereo && view.colorRt.extent.depth > 1)
+	{
+		const auto rcd = createRayCameraData(view.cameras[1], view.colorRt.extent);
+		owlParamsSetRaw(launchParameters_, "cameraData", &rcd);
+		owlParamsSetRaw(launchParameters_, "surfacePointer", &cudaSurfaceObjects[1]);
+		owlAsyncLaunch2D(rayGen_, view.colorRt.extent.width, view.colorRt.extent.height, launchParameters_);
+	}
+
 	
-	// Destroy and unmap
 	{
 		for (auto i = 0; i < view.colorRt.extent.depth; i++)
 		{
@@ -136,16 +159,18 @@ auto SimpleTrianglesRenderer::onRender(const View& view) -> void
 	signalParams.flags = 0;
 	signalParams.params.fence.value = view.fenceValue;
 	cudaRet = cudaSignalExternalSemaphoresAsync(&initializationInfo_.waitSemaphore, &signalParams, 1);
-
 }
 
 auto SimpleTrianglesRenderer::onInitialize() -> void
 {
-	trs = affine3f::translate({ 0, 0, 0 }).scale({ 1, 1, 1 });
 	RendererBase::onInitialize();
+
+	trs_ = affine3f::translate({ 0, 0, 0 }).scale({ 1, 1, 1 });
+
 	// create a context on the first device:
-	context = owlContextCreate(nullptr, 1);
-	OWLModule module = owlModuleCreate(context, SimpleTrianglesDeviceCode_ptx);
+	context_ = owlContextCreate(nullptr, 1);
+	auto module = owlModuleCreate(context_, SimpleTrianglesDeviceCode_ptx);
+
 
 	// ##################################################################
 	// set up all the *GEOMETRY* graph we want to render
@@ -158,23 +183,20 @@ auto SimpleTrianglesRenderer::onInitialize() -> void
 									   { "vertex", OWL_BUFPTR, OWL_OFFSETOF(TrianglesGeomData, vertex) },
 									   { "color", OWL_FLOAT4, OWL_OFFSETOF(TrianglesGeomData, color) } };
 	OWLGeomType trianglesGeomType =
-		owlGeomTypeCreate(context, OWL_TRIANGLES, sizeof(TrianglesGeomData), trianglesGeomVars, 3);
+		owlGeomTypeCreate(context_, OWL_TRIANGLES, sizeof(TrianglesGeomData), trianglesGeomVars, 3);
 	owlGeomTypeSetClosestHit(trianglesGeomType, 0, module, "TriangleMesh");
 
 	// ##################################################################
 	// set up all the *GEOMS* we want to run that code on
 	// ##################################################################
 
-
 	// ------------------------------------------------------------------
 	// triangle mesh
 	// ------------------------------------------------------------------
-	OWLBuffer vertexBuffer = owlDeviceBufferCreate(context, OWL_FLOAT3, NUM_VERTICES, vertices);
-	OWLBuffer indexBuffer = owlDeviceBufferCreate(context, OWL_INT3, NUM_INDICES, indices);
-	// OWLBuffer frameBuffer
-	//   = owlHostPinnedBufferCreate(context,OWL_INT,fbSize.x*fbSize.y);
+	OWLBuffer vertexBuffer = owlDeviceBufferCreate(context_, OWL_FLOAT3, NUM_VERTICES, vertices);
+	OWLBuffer indexBuffer = owlDeviceBufferCreate(context_, OWL_INT3, NUM_INDICES, indices);
 
-	OWLGeom trianglesGeom = owlGeomCreate(context, trianglesGeomType);
+	OWLGeom trianglesGeom = owlGeomCreate(context_, trianglesGeomType);
 
 	owlTrianglesSetVertices(trianglesGeom, vertexBuffer, NUM_VERTICES, sizeof(vec3f), 0);
 	owlTrianglesSetIndices(trianglesGeom, indexBuffer, NUM_INDICES, sizeof(vec3i), 0);
@@ -186,83 +208,57 @@ auto SimpleTrianglesRenderer::onInitialize() -> void
 	// ------------------------------------------------------------------
 	// the group/accel for that mesh
 	// ------------------------------------------------------------------
-	OWLGroup trianglesGroup = owlTrianglesGeomGroupCreate(context, 1, &trianglesGeom);
+	OWLGroup trianglesGroup = owlTrianglesGeomGroupCreate(context_, 1, &trianglesGeom);
 	owlGroupBuildAccel(trianglesGroup);
-	world_ = owlInstanceGroupCreate(context, 1, &trianglesGroup, nullptr, nullptr, OWL_MATRIX_FORMAT_OWL, OPTIX_BUILD_FLAG_ALLOW_UPDATE);
+	world_ = owlInstanceGroupCreate(context_, 1, &trianglesGroup, nullptr, nullptr, OWL_MATRIX_FORMAT_OWL,
+									OPTIX_BUILD_FLAG_ALLOW_UPDATE);
 	owlGroupBuildAccel(world_);
-
-	// ##################################################################
-	// set miss and raygen program required for SBT
-	// ##################################################################
 
 	// -------------------------------------------------------
 	// set up miss prog
 	// -------------------------------------------------------
-	OWLVarDecl missProgVars[] = { { "color0", OWL_FLOAT4, OWL_OFFSETOF(MissProgData, color0) },
-								  { "color1", OWL_FLOAT4, OWL_OFFSETOF(MissProgData, color1) },
-								  { /* sentinel to mark end of list */ } };
-	// ----------- create object  ----------------------------
-	OWLMissProg missProg = owlMissProgCreate(context, module, "miss", sizeof(MissProgData), missProgVars, -1);
+	{
+		OWLVarDecl missProgVars[] = { { "color0", OWL_FLOAT4, OWL_OFFSETOF(MissProgData, color0) },
+									  { "color1", OWL_FLOAT4, OWL_OFFSETOF(MissProgData, color1) },
+									  { /* sentinel to mark end of list */ } };
+		// ----------- create object  ----------------------------
+		missProg_ = owlMissProgCreate(context_, module, "miss", sizeof(MissProgData), missProgVars, -1);
 
-	// ----------- set variables  ----------------------------
-	owlMissProgSet4f(missProg, "color0", owl4f{ .0f, 0.f, 0.f, 0.0f });
-	owlMissProgSet4f(missProg, "color1", owl4f{ .0f, .0f, .0f, 0.0f });
+		// ----------- set variables  ----------------------------
+		owlMissProgSet4f(missProg_, "color0", owl4f{ .0f, 0.f, 0.f, 0.0f });
+		owlMissProgSet4f(missProg_, "color1", owl4f{ .0f, .0f, .0f, 0.0f });
+	}
 
 	// -------------------------------------------------------
 	// set up ray gen program
 	// -------------------------------------------------------
-	OWLVarDecl rayGenVars[] = { 
-								{ "fbSize", OWL_INT2, OWL_OFFSETOF(RayGenData, fbSize) },
-								{ "world", OWL_GROUP, OWL_OFFSETOF(RayGenData, world) },
-								{ /* sentinel to mark end of list */ } };
-
-	OWLVarDecl launchParamsVarsWithStruct[] = { { "cameraData", OWL_USER_TYPE(RayCameraData), OWL_OFFSETOF(MyLaunchParams, cameraData) },
-												{ "surfacePointer" ,OWL_USER_TYPE(cudaSurfaceObject_t), OWL_OFFSETOF(MyLaunchParams, surfacePointer) },
-													{}
-	};
-
-	launchParameters_ = owlParamsCreate(context, sizeof(MyLaunchParams), launchParamsVarsWithStruct, -1);
-
-	// Create Buffer for Surfaceobject
-	// TODO: Does not work if we have 2 colorRT in view
-	surfaceBuffer_ = owlDeviceBufferCreate(context, OWL_USER_TYPE(cudaSurfaceObject_t), 1, nullptr);
-
-
-	// ----------- create object  ----------------------------
-	rayGen = owlRayGenCreate(context, module, "simpleRayGen", sizeof(RayGenData), rayGenVars, -1);
-	/* camera and frame buffer get set in resiez() and cameraChanged() */
-	owlRayGenSetGroup(rayGen, "world", world_);
-
-	// ##################################################################
-	// build *SBT* required to trace the groups
-	// ##################################################################
-
-	owlBuildPrograms(context);
-	owlBuildPipeline(context);
-	owlBuildSBT(context);
-}
-
-namespace
-{
-	struct GuiData
 	{
-		struct BackgroundColorPalette
-		{
-			std::array<float, 4> color1{ 0.572f, 0.100f, 0.750f,1.0f };
-			std::array<float, 4> color2{ 0.0f, 0.3f, 0.3f, 1.0f };
-		};
-		BackgroundColorPalette rtBackgroundColorPalette;
+		OWLVarDecl rayGenVars[] = { 
+									{ "fbSize", OWL_INT2, OWL_OFFSETOF(RayGenData, fbSize) },
+									{ "world", OWL_GROUP, OWL_OFFSETOF(RayGenData, world) },
+									{ /* sentinel to mark end of list */ } };
 
-		struct CubeVolumeTransform
-		{
-			std::array<float, 3> position{ 0, 0, 0 };
-			std::array<float, 3> scale{ 1, 1, 1 };
-			std::array<float, 3> rotation{ 0, 0, 0 };
-		};
-		CubeVolumeTransform rtCubeVolumeTransform{};
-	};
+		rayGen_ = owlRayGenCreate(context_, module, "simpleRayGen", sizeof(RayGenData), rayGenVars, -1);
 
-	GuiData guiData{};
+		owlRayGenSetGroup(rayGen_, "world", world_);
+	}
+
+	// -------------------------------------------------------
+	// set up launch params
+	// -------------------------------------------------------
+	{
+		OWLVarDecl launchParamsVarsWithStruct[] = {
+			{ "cameraData", OWL_USER_TYPE(RayCameraData), OWL_OFFSETOF(MyLaunchParams, cameraData) },
+			{ "surfacePointer", OWL_USER_TYPE(cudaSurfaceObject_t), OWL_OFFSETOF(MyLaunchParams, surfacePointer) },
+			{}
+		};
+
+		launchParameters_ = owlParamsCreate(context_, sizeof(MyLaunchParams), launchParamsVarsWithStruct, -1);
+	}
+
+	owlBuildPrograms(context_);
+	owlBuildPipeline(context_);
+	owlBuildSBT(context_);
 }
 
 auto SimpleTrianglesRenderer::onGui() -> void
@@ -274,7 +270,7 @@ auto SimpleTrianglesRenderer::onGui() -> void
 	ImGui::SeparatorText("CubeVolume Transform");
 	ImGui::DragFloat3("Position", guiData.rtCubeVolumeTransform.position.data(), 0.01f, -FLT_MAX, FLT_MAX);
 	ImGui::DragFloat3("Scale", guiData.rtCubeVolumeTransform.scale.data(), 0.01f, 0.01f, 100.0f);
-	ImGui::DragFloat3("Rotation", guiData.rtCubeVolumeTransform.rotation.data(), 0.01f, 0.0f, FLT_MAX);
+	ImGui::DragFloat3("Rotation", guiData.rtCubeVolumeTransform.rotation.data(), 0.01f, -FLT_MAX, FLT_MAX);
 	if (ImGui::Button("Reset Transform", ImVec2(ImGui::GetContentRegionAvail().x, 0)))
 	{
 		guiData.rtCubeVolumeTransform = {};
