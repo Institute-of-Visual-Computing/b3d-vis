a5ba4ba13aa380b5e3e9deb544d8716473943ad8
Anton <anton.kindsvater@googlemail.com>
2024-04-11 18:37:16 +0200

    Add runtime volume selection and test GUI

--- renderer/nano/RuntimeDataSet.h
+++ renderer/nano/RuntimeDataSet.h
@@ -2,7 +2,6 @@
 #include <filesystem>
 #include <vector>
 
-
 #include <SharedStructs.h>
 
 namespace b3d::renderer::nano
@@ -23,13 +22,18 @@ namespace b3d::renderer::nano
 		owl::AffineSpace3f renormalizeScale{};
 	};
 
-	class RuntimeDataSet
+	class RuntimeDataSet final
 	{
 	public:
 		RuntimeDataSet();
 		auto select(std::size_t index) -> void;
 		auto getSelectedData() -> RuntimeVolume&;
+		auto addNanoVdb(const std::filesystem::path& path) -> void;
+		auto addNanoVdb(const NanoVdbVolume& volume) -> void;
 
+		RuntimeDataSet(RuntimeDataSet&) = delete;
+		RuntimeDataSet(RuntimeDataSet&&) = default;
+		~RuntimeDataSet();
 	private:
 		std::vector<std::filesystem::path> loadableVolumes_{};
 		std::vector<RuntimeVolume> runtimeVolumes_{};
