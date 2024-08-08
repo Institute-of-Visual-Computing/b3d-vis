e1e90d42ba4c469cb4003d9527f0ade47ad4e794
DESKTOP-NVMDPV9\david <d3ibich@gmail.com>
2024-08-08 07:35:17 +0200

    WIP: Refactor Tools
    Cleanup for structured project

--- tools/projectExplorer/src/Common.cpp
+++
@@ -1,9 +0,0 @@
-#include "uuid.h"
-
-
-namespace b3d::tools::projectexplorer
-{
-	uuids::uuid_name_generator gNameGenerator{
-		uuids::uuid::from_string("123456789-abcdef-123456789-abcdef-12").value()
-	};
-}
--- tools/projectExplorer/src/Common.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace b3d::tools::projectexplorer
-{
-	extern uuids::uuid_name_generator gNameGenerator;
-}
